f951303b9ac25660472ba1e53076bb72bdac88c6
Ricardo Delgado <ricdelgado80@gmail.com>
2022-05-31 08:45:11 -0400

    remove firebase

--- src/main.js
+++ src/main.js
@@ -1,23 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { initializeApp } from 'firebase/app'
-import { getAuth, signInAnonymously } from 'firebase/auth'
 import './assets/scss/global.scss'
-
-initializeApp({
-  apiKey: 'AIzaSyD2trh3VfGZ3ALVbFKrJe7wVENC-ze4y1g',
-  authDomain: 'portfolio-images-580ff.firebaseapp.com',
-  projectId: 'portfolio-images-580ff',
-  storageBucket: 'portfolio-images-580ff.appspot.com',
-  messagingSenderId: '723295325231',
-  appId: '1:723295325231:web:76a26c96e934e19bc58ada'
-})
-
-const auth = getAuth()
-signInAnonymously(auth)
-  .then(() => console.log('signed in'))
-  .catch(console.warn)
 
 const app = createApp(App)
 
