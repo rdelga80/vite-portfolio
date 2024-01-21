f9056830699a825fe61d4d165990390a8a92f89f
Ricardo Delgado <ricdelgado80@gmail.com>
2024-01-21 12:45:27 -0500

    add syntax highlighting

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,27 @@ import vue from '@vitejs/plugin-vue'
 
 import mdPlugin, { Mode } from 'vite-plugin-markdown'
 import GlobPlugin from 'vite-plugin-glob'
+import hljs from 'highlight.js'
 
 const srcPath = new URL('./src', import.meta.url)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), mdPlugin({ mode: [Mode.VUE, Mode.HTML], markdownIt: {
-    html: true,
-  }
+  plugins: [vue(), mdPlugin({
+    mode: [Mode.VUE, Mode.HTML],
+    markdownIt: {
+      html: true,
+      highlight: (str, lang) => {
+        if (!lang) {
+          return str
+        }
+
+        return hljs.highlight(
+          str,
+          { language: lang }
+        ).value
+      }
+    }
   }), GlobPlugin()],
   resolve: {
     alias: {
