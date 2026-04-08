d7829805d0bd1fce255d08a94c57d689a86e691b
Ricardo Delgado <ricdelgado80@gmail.com>
2026-04-08 10:20:34 -0400

    add preloader to homepage

--- src/pages/index/+Page.vue
+++ src/pages/index/+Page.vue
@@ -1,13 +1,50 @@
 <script setup>
-import ASidebar from '@/components/ASidebar.vue'
-import ArticleLoader from '@/components/ArticleLoader.vue'
-import { getArticles } from '@/assets/helpers.js'
+import { useData } from 'vike-vue/useData'
+import ALink from '@/components/ALink.vue'
+import { articleSlugToLink } from '@/assets/helpers'
 
-const articles = getArticles(5)
+const { articles } = useData()
 </script>
 
 <template>
-  <div v-for="(articleSlug, index) in articles" :key="index">
-    <ArticleLoader :article-slug="articleSlug" summary />
-  </div>
+  <article v-for="{ slug, attributes, summary } in articles" :key="slug" class="article">
+    <ALink :href="articleSlugToLink(slug)">
+      <h2 class="article-title">{{ attributes.title }}</h2>
+    </ALink>
+    <span class="article-date">{{ attributes.date }}</span>
+    <div class="summary">
+      <div v-html="summary" />
+    </div>
+    <div class="more">
+      <ALink :href="articleSlugToLink(slug)">Read More...</ALink>
+    </div>
+  </article>
 </template>
+
+<style scoped>
+.article {
+  &:deep(img) {
+    width: 100%;
+  }
+  &:deep(pre) {
+    white-space: pre-wrap;
+  }
+}
+
+.article-title {
+  margin-bottom: 0;
+}
+
+.article-title a {
+  text-decoration: none;
+}
+
+.article-date {
+  font-size: 0.9rem;
+  font-style: italic;
+}
+
+.more {
+  text-align: right;
+}
+</style>
